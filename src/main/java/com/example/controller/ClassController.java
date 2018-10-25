e95c4557fa2bc7d00533db863c2376f821aba52d
ellen <814989031@qq.com>
2018-10-25 20:40:26 +0800

    spring boot整合mybatis-plus

--- src/main/java/com/example/controller/ClassController.java
+++ src/main/java/com/example/controller/ClassController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
+ * 班级Controller
  * @author ellen
  * @version 1.0
  * @date 2018/10/25
--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 学生Controller
  * @author ellen
  * @version 1.0
  * @date 2018/10/25
